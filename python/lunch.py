773297c28f4ed9d45250261c3ed35fa64b92d85c
TerryPatterson <Terryp@wegrok.net>
2018-02-07 10:21:25 -0500

    Improved Menu movement

--- python/lunch.py
+++ python/lunch.py
@@ -63,7 +63,7 @@ def main_menu():
               "\t==========================================\n"
               "\t0: to quit.\n"
                "\t1: to manage restaurants.")
-        display_resuarants()
+        display_resuarants(2)
         user_input = input(f"\tI would like to: ")
         if vaildate_input(user_input, 0, len(restaurants)+1):
             user_choice = int(user_input)
@@ -83,6 +83,7 @@ def main_menu():
                 if weeks[week][restaurant] < 3:
                     restaurants[restaurant] += 1
                     weeks[week][restaurant] += 1
+                    sort()
 
                 else:
                     print(f"\tI am sorry you have been to {restaurant} {weeks[week][restaurant]} times in a week.\n"
@@ -93,6 +94,7 @@ def main_menu():
             else:
                 weeks[week][restaurant] = 1
                 restaurants[restaurant] += 1
+                sort()
 
         elif user_choice == 1:
             clear()
@@ -132,7 +134,7 @@ def managment_menu():
                "\t3: delete a restaurant.\n"
                "\t4: add a restaurant.\n"
                "\t5: go back.")
-        user_input = input("I would like to: ")
+        user_input = input(f"\tI would like to: ")
         if not vaildate_input(user_input, 0, 5):
                 print(f"\tPlease select either 0 through 2 it change sorting,\n"
                         "\t3 to delete a restaurant, or 4 to add a restaurant.\n"
@@ -144,12 +146,24 @@ def managment_menu():
         if user_choice == 0:
             sort_type = 0
             sort()
+            clear()
+            display_resuarants(0)
+            print(f"\tSort done. Hit enter to continue.")
+            input()
         if user_choice == 1:
             sort_type = 1
             sort()
+            clear()
+            display_resuarants(0)
+            print(f"\tSort done. Hit enter to continue.")
+            input()
         if user_choice == 2:
             sort_type = 2
             sort()
+            clear()
+            display_resuarants(0)
+            print(f"\tSort done. Hit enter to continue.")
+            input()
         if user_choice == 3:
             remove_restaurant()
         if user_choice == 4:
@@ -178,9 +192,9 @@ def remove_restaurant():
     while choosing:
         clear()
         print("\tWhich restaurant would you like to delete?\n"
-              "\t=========================================="
+              "\t==========================================\n"
               "\t0:Go back to previous menu")
-        display_resuarants()
+        display_resuarants(1)
         user_input = input(f"\tI would like to delete: ")
         if vaildate_input(user_input, 0, len(restaurants)):
             user_choice = int(user_input)
@@ -197,20 +211,26 @@ def remove_restaurant():
             user_choice -= 1
             del restaurants[sorted_restaurants[user_choice]]
             sort()
-            print(f"\tRestaurant deleted hit enter to return to the main menu")
+            clear()
+            print(f"\tRestaurant deleted hit enter to return to the previous menu")
+            display_resuarants(0)
             input()
             choosing = False
 def add_restuarant():
     clear()
     print(f"\tThe current list is:")
-    display_resuarants()
+    display_resuarants(0)
     user_input = input(f"\tWhat is the name of the new restaurant? ")
     restaurants[user_input] = 0
     sort()
-def display_resuarants():
+    clear()
+    print(f"\t{user_input} added to restaurant list.")
+    display_resuarants(0)
+    input(f"\tHit enter to return to the previous menu.")
+def display_resuarants(offset):
     for i in range(len(restaurants)):
         name = sorted_restaurants[i]
-        print(f"\t{i+2}: {name} visited {restaurants[name]}")
+        print(f"\t{i+offset}: {name} visited {restaurants[name]}")
 
 
 main_menu()
